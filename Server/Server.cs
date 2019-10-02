73f3ad0691bd698f832c0247e2bde17e334e0a2d
Arina <pavlova.arina.e@gmail.com>
2019-10-02 12:51:34 +0300

    move qa and commands to file

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -11,25 +12,29 @@ namespace ChatBot
 {
     class Server
     {
-        static readonly List<ChatClient> clients = new List<ChatClient>();
+        private static readonly List<ChatClient> Clients = new List<ChatClient>();
         private TcpListener server = null;
-        private string getAllUsersCommand = "кто здесь";
-
-        private static Dictionary<string, string> questionsAndAnswers = new Dictionary<string, string>()
-        {
-            ["как дела"] = "Хорошо",
-            ["чай или кофе"] = "Кофе",
-            ["кошки или собаки"] = "Кошки",
-            ["табы или пробелы"] = "Пффф",
-            ["лыжи или сноуборд"] = "Сноуборд",
-            ["сложно отвечать на вопросы"] = "Очень"
-        };
+        private static Dictionary<string, string> questionsAndAnswers = new Dictionary<string, string>();
+        private static Dictionary<string, string> commands = new Dictionary<string, string>();
 
         private readonly string goodbyeString = "пока";
+        private readonly string getAllUsersString = "кто здесь";
+        private readonly string helloString = "А я бот Борис и меня можно спросить о самых важных вещах:";
+
         public Server(string ip, int port)
         {
-            IPAddress localAddr = IPAddress.Parse(ip);
-            server = new TcpListener(localAddr, port);
+            var lines = File.ReadAllLines(@"questions.txt").Select(x => x.Split(';')).ToList();
+            for (int i = 0; i < lines.Count; i ++)
+            {
+                questionsAndAnswers.Add(lines[i][0], lines[i][1]);
+            }
+            lines = File.ReadAllLines(@"commands.txt").Select(x => x.Split(';')).ToList();
+            for (int i = 0; i < lines.Count; i ++)
+            {
+                commands.Add(lines[i][0], lines[i][1]);
+            }
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            server = new TcpListener(ipAddress, port);
             server.Start();
             StartListener();
         }
@@ -42,9 +47,9 @@ namespace ChatBot
                 {
                     Console.WriteLine("Waiting for a connection...");
                     var client = new ChatClient(server.AcceptTcpClient());
-                    clients.Add(client);
+                    Clients.Add(client);
                     Console.WriteLine("Connected!");
-                    Thread t = new Thread(new ParameterizedThreadStart(HandleDeivce));
+                    Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
                     t.Start(client);
                 }
             }
@@ -55,7 +60,7 @@ namespace ChatBot
             }
         }
 
-        public void HandleDeivce(Object obj)
+        public void HandleClient(Object obj)
         {
             ChatClient chatClient = (ChatClient)obj;
             var stream = chatClient.Client.GetStream();
@@ -63,58 +68,70 @@ namespace ChatBot
             Byte[] hello = System.Text.Encoding.UTF8.GetBytes(helloString);
             stream.Write(System.Text.Encoding.UTF8.GetBytes(helloString), 0, hello.Length);
             Byte[] bytes = new Byte[256];
-            int i;
             bool isFirstAnswer = true;
             try
             {
+                int i;
                 while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
                     var question = Encoding.UTF8.GetString(bytes, 0, i);
                     Regex rgx = new Regex("[^a-zA-Zа-яА-Я0-9 -]");
                     question = rgx.Replace(question.ToLower(), "");
                     if (question == goodbyeString) break;
-                    if (!isFirstAnswer)
-                    {
-                        if (question == getAllUsersCommand)
-                        {
-                            foreach (var client in clients)
-                            {
-                                Byte[] reply = System.Text.Encoding.UTF8.GetBytes(client.Name.First().ToString().ToUpper() + client.Name.Substring(1));
-                                stream.Write(reply, 0, reply.Length);
-                            }
-                        }
-                        else
-                        {
-                            string answer = questionsAndAnswers.ContainsKey(question)
-                                ? questionsAndAnswers[question]
-                                : "whatever";
-                            Byte[] reply = System.Text.Encoding.UTF8.GetBytes(answer);
-                            stream.Write(reply, 0, reply.Length);
-                        }
-                    }
-                    else
-                    {
-                        isFirstAnswer = false;
-                        chatClient.Name = question;
-                        string answer = "А я бот Борис и меня можно спросить о самых важных вещах:\r\n";
-                        foreach (var qa in questionsAndAnswers)
-                        {
-                            answer += qa.Key.First().ToString().ToUpper() + qa.Key.Substring(1) + "?\r\n";
-                        }
-                        answer += "напиши \"кто здесь\", чтобы получить список присоединенных пользователей\r\n";
-                        Byte[] reply = System.Text.Encoding.UTF8.GetBytes(answer);
-                        stream.Write(reply, 0, reply.Length);
-                    }
-                }
+                    CreateAndSendAnswer(chatClient, question, ref isFirstAnswer);
+                }             
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception: {0}", e.ToString());
+            }
+            finally
+            {
+                Clients.Remove(chatClient);
+                chatClient.Client.Client.Shutdown(SocketShutdown.Both);
                 chatClient.Client.Close();
             }
-            clients.Remove(chatClient);
-            chatClient.Client.Client.Shutdown(SocketShutdown.Both);
-            chatClient.Client.Close();
+        }
+
+        private void CreateAndSendAnswer(ChatClient chatClient, string question,ref bool isFirstAnswer)
+        {
+            var stream = chatClient.Client.GetStream();
+            if (!isFirstAnswer)
+            {
+                if (question == getAllUsersString)
+                {
+                    foreach (var client in Clients) Answer(stream, client.Name);
+                }
+                else
+                {
+                    string answer = questionsAndAnswers.ContainsKey(question)
+                        ? questionsAndAnswers[question]
+                        : "whatever";
+                    Answer(stream, answer);
+                }
+            }
+            else
+            {
+                isFirstAnswer = false;
+                chatClient.Name = question.First().ToString().ToUpper() + question.Substring(1);
+                string answer = helloString +"\r\n";
+                foreach (var (key, _) in questionsAndAnswers)
+                {
+                    answer += key.First().ToString().ToUpper() + key.Substring(1) + "?\r\n";
+                }
+                answer += "\r\n";
+                foreach (var (key, value) in commands)
+                {
+                    answer += "Введи " + key + ", чтобы " + value + "\r\n";
+                }
+                Answer(stream, answer);
+            }
+        }
+
+        private void Answer(NetworkStream stream, string answer)
+        {
+            Byte[] reply = System.Text.Encoding.UTF8.GetBytes(answer);
+            stream.Write(reply, 0, reply.Length);
         }
     }
 }
